88bc660e9cd88c663aa885b6cdc172960063b49d
Eric <eric_3991@hotmail.com>
2022-09-15 15:32:09 +0200

    second commit

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -26,7 +26,7 @@ function Formulario({ cliente, cargando }) {
     try {
       let respuesta;
       if (cliente.id) {
-        const url = `http://localhost:4000/clientes/${cliente.id}`;
+        const url = `${import.meta.env.VITE_API_URL}/${cliente.id}`;
         respuesta = await fetch(url, {
           method: "PUT",
           body: JSON.stringify(valores),
@@ -35,7 +35,7 @@ function Formulario({ cliente, cargando }) {
           },
         });
       } else {
-        const url = "http://localhost:4000/clientes";
+        const url = import.meta.env.VITE_API_URL;
         respuesta = await fetch(url, {
           method: "POST",
           body: JSON.stringify(valores),
