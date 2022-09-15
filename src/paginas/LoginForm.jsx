4c4e1f8a40da6470e86e1c8ce9a47fbae1277edc
Eric <eric_3991@hotmail.com>
2022-09-15 14:47:34 +0200

    first commit

---
+++ src/paginas/LoginForm.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function LoginForm() {
+  return <h1>Formulario</h1>;
+}
+
+export default LoginForm;
---
+++ src/paginas/VerCliente.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Spinner from "../components/Spinner";
+
+const VerCliente = () => {
+  const [cargando, setCargando] = useState(true);
+  const [cliente, setCliente] = useState({});
+  const { id } = useParams();
+  useEffect(() => {
+    const obtenerClienteAPI = async () => {
+      try {
+        const url = `http://localhost:4000/clientes/${id}`;
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+        setCliente(resultado);
+      } catch (error) {}
+      setCargando(!cargando);
+    };
+    obtenerClienteAPI();
+  }, []);
+
+  return cargando ? (
+    <Spinner />
+  ) : Object.keys(cliente).length === 0 ? (
+    <p>No hay resultados</p>
+  ) : (
+    <div>
+      <p className="text-4xl text-gray-600">
+        <span className=" uppercase font-bold "> Cliente : </span>
+        {cliente.nombre}
+      </p>
+      {cliente.empresa && (
+        <p className="text-2xl text-gray-600  ">
+          <span className=" uppercase font-bold ">Empresa : </span>
+          {cliente.empresa}
+        </p>
+      )}
+      {cliente.telefono && (
+        <p className="text-2xl text-gray-600">
+          <span className=" uppercase font-bold ">Telefono : </span>
+          {cliente.telefono}
+        </p>
+      )}
+      <p className="text-2xl text-gray-600">
+        <span className=" uppercase font-bold ">Email : </span>
+        {cliente.email}
+      </p>
+      {cliente.notas && (
+        <p className="text-2xl text-gray-600">
+          <span className=" uppercase font-bold ">Notas :</span> {cliente.notas}
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default VerCliente;
